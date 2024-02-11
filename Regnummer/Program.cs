8da5b8d33f268c6b99b9a7a6b50afe3211c0bc42
EFakLloyd <elisabeth.fak.lloyd@gmail.com>
2024-02-11 17:17:26 +0100

    giltigt regnummer

---
+++ Regnummer/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program{
+  static void Main (string[] args){
+
+Console.WriteLine("Skriv in ett registreringsnummer: ");
+String regnr = Console.ReadLine();
+
+if (ValideringAvRegnr(regnr)){
+Console.WriteLine("Ja");
+  }
+else{
+  Console.WriteLine("Nej");
+  }
+}
+
+
+static bool ValideringAvRegnr(string regnr){
+  if (regnr.Length !=6){
+    return false;
+  }
+  string bokstav = regnr.Substring(0, 3);
+  string siffra = regnr.Substring(3);
+
+  
+
+  if(char.IsLetter(bokstav[0])&&char.IsLetter(bokstav[1])&&char.IsLetter(bokstav[2])&& 
+  char.IsDigit(siffra[0])&& char.IsDigit(siffra[1])&& char.IsDigit(siffra[2])){
+    return true;
+
+  }
+if(char.IsLetter(bokstav[0])&&char.IsLetter(bokstav[1])&&char.IsLetter(bokstav[2])&& 
+  char.IsDigit(siffra[0])&& char.IsDigit(siffra[1])&& char.IsLetter(bokstav[2])){
+    return true;
+  }
+return false;
+}}
